d2649ec8fc448006c4c666f577f59fe970433ae5
Austin Vigo <avigo@uci.edu>
2019-09-19 16:38:57 -0700

    navigation is hooked up

--- nutritients/src/App.js
+++ nutritients/src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
-import {
-  View,
-  Text
-} from 'react-native';
+import { View, Text } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-const App = () => {
-  return(
-    <View/>
-  );
-};
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
 
-export default App;
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>LoginScreen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Login: {
+    screen: LoginScreen
+  },
+  {
+    intialRouteName: "Login"
+  }
+});
+
+export default createAppContainer(AppNavigator);
