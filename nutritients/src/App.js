0bd4cfca3da5e632ab1dcb49fcd807689a69da17
Austin Vigo <avigo@uci.edu>
2019-09-19 16:41:31 -0700

    reset the App.js
    reset the App.js

--- nutritients/src/App.js
+++ nutritients/src/App.js
@@ -1,38 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {
+  View
+} from 'react-native';
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
-}
+const App = () => {
+  return(
+    <View/>
+  );
+};
 
-class LoginScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>LoginScreen</Text>
-      </View>
-    );
-  }
-}
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  },
-  Login: {
-    screen: LoginScreen
-  },
-  {
-    intialRouteName: "Login"
-  }
-});
-
-export default createAppContainer(AppNavigator);
+export default App;
