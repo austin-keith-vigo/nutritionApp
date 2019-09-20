e00b9c6f48c0638037f87a3c74c9fc5f13f8d8e8
Austin Vigo <avigo@uci.edu>
2019-09-20 14:54:34 -0700

    testing api
    testing api

--- nutritients/src/screens/HomeScreen.js
+++ nutritients/src/screens/HomeScreen.js
@@ -11,9 +11,27 @@ import {
   SearchResultsList
 } from './../components';
 
+const API_KEY = "e55c80982c814318ba1ad19d7708978e";
+
 //Houses a search button and list to present
 //values of the search button
 class HomeScreen extends Component{
+
+  constructor(props){
+    super(props);
+
+    console.log('testing spoonacular');
+
+    fetch("https://api.spoonacular.com/recipes/search?apiKey=e55c80982c814318ba1ad19d7708978e&query=cheese&number=2")
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson.results);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  }
   render(){
     return(
       <View style={styles.viewStyle}>
